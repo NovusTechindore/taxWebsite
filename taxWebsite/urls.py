f001de1b55f4323cf6b613d187c8a72e3ef7dc82
NovusTechindore <56116018+NovusTechindore@users.noreply.github.com>
2020-10-02 17:10:49 +0530

    Update urls.py

--- taxWebsite/urls.py
+++ taxWebsite/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.url import path
 
-urlpatterns = [
+urlpattern = [
     path('admin/', admin.site.urls),
     path('',include('advocate.urls')),
 ]
